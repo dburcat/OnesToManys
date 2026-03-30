7f5ef267e09e239f5a622f9e25f1ad999c4e5c4c
david <dburcad@gmail.com>
2026-03-30 16:39:03 -0400

    refreshed dev branch

--- test_fk_constraint.sql
+++
@@ -1,5 +0,0 @@
--- Test referential integrity constraints
-PRAGMA foreign_keys = ON;
-
--- Test 1: Try to insert Equipment with invalid armor_id (should fail)
-INSERT INTO Equipment (armor_id, weapon_id, cape_id) VALUES (999, 1, 1);
